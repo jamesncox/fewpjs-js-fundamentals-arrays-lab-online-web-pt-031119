ffc53714d1f265f574243e3f65d904e003396016
James Cox <jamesncox@yahoo.com>
2019-09-30 20:31:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ append.push("Odie");
 prepend.unshift("Odie");
 removeLast.pop();
 removefirst.shift();
-// 
+//
 // const append = ["Milo", "Otis", "Garfield"];
 // const prepend = ["Milo", "Otis", "Garfield"];
 // const removeLast = ["Milo", "Otis", "Garfield"];
 // const removeFirst = ["Milo", "Otis", "Garfield"];
-// 
+//
 // append.push("Odie");
 // prepend.unshift("Odie");
 // removeLast.pop();
